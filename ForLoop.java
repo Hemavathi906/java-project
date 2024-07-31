a524f8f8c22e57b9a018d54418c3690ee842032b
Hemavathi906 <hemavathia08@gmail.com>
2024-07-31 20:27:05 +0530

    ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("for loop");
+        int i;
+    for( i=0;i<=7;i++){
+        System.err.println("for loop");
+    }
+    }
+    
+    
+}
