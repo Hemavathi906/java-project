a6ef043e5e5f7158d1cde5877e5426846ef12571
Hemavathi906 <hemavathia08@gmail.com>
2024-07-31 20:44:14 +0530

    DoWhileLoop.java

---
+++ DoWhileLoop.java
@@ -0,0 +1,10 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i=3;
+        do{
+            System.out.println("dowhile loop");
+        }while(i<0);
+
+    }
+    
+}
