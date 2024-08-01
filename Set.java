4acd53125519b8461c47797302838b5da405a6df
Hemavathi906 <hemavathia08@gmail.com>
2024-08-01 21:21:50 +0530

    Set.java

--- Set.java
+++ Set.java
@@ -1,4 +1,7 @@
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class Set {
     public static void main(String[] args) {
@@ -8,8 +11,29 @@ public class Set {
         hs.add(15);
         hs.add(20);
         hs.add(25);
-        hs..add(25);
+        hs.add(25);
         System.out.println(hs);
-    }
+
+        LinkedHashSet ls =new LinkedHashSet();
+        ls.add(3);
+        ls.add(4);
+        ls.add(15);
+        ls.add(24);
+        ls.add(6);
+        ls.add(3);
+        System.out.println(ls);
+
+        TreeSet ts= new TreeSet();
+        ts.add(3);
+        ts.add(4);
+        ts.add(15);
+        ts.add(6);
+        ts.add(24);
+        ts.add(3);
+        System.out.println(ts);
+
+
+
+            }
     
 }
