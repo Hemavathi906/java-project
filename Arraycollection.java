e27df35e9204fd540635daa26a73f5a7fd020b3e
Hemavathi906 <hemavathia08@gmail.com>
2024-08-01 20:10:19 +0530

    Arraycollection.java

---
+++ Arraycollection.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+public class Arraycollection {
+    public static void main(String[] args) {
+        ArrayList al =new ArrayList();
+        al.add(4);
+        al.add(5);
+        al.add(6);
+        al.add(7);
+        al.add("hema");
+        al.add(4,"subi");
+        System.out.println(al);
+        System.out.println(al.add(8));
+        System.out.println((al));
+        System.out.println(al);
+        System.out.println(al.contains("hema"));
+        System.out.println(al.get(0));
+        System.out.println(al.remove(0));
+
+        
+    }
+    
+}
